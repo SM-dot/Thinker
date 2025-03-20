bdf3bcda388ba7d26f35f165456f059acacf16d2
SM-dot <66779569+SM-dot@users.noreply.github.com>
2025-03-20 12:13:55 -0400

    Update GraphValidTree.py
    Added BFS

--- GraphValidTree.py
+++ GraphValidTree.py
@@ -3,6 +3,8 @@
 # Category: Trees and Graphs 
 
 
+# DFS 
+
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         # the main thing is that there should not be a cycle, if there 
@@ -38,4 +40,51 @@ class Solution:
             return False 
         
         return not cycle_detection(0, -1) and len(visit) == n
+
+
+# BFS: 
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # the main thing is that there should not be a cycle, if there 
+        # is a cycle means that we can classify it as a graph
+        # if multiple components or 2 trees, still incorrect as we are only checking for 1 tree
+        # Will have n nodes, start a traversal from the 0th node
+        # If you find no cycle and visited nodes == n => all good it is a tree
+        # If you fing no cycle and visited nodes != n => 2 or more components/ trees therefor return false
+
+        # if no nodes in a tree, it can still be considered a tree
+        if not n:
+            return True
+        visit = set()
+        # hashmap
+        adj = [[] for i in range(n)]
+        # populating the hashmap
+        for edge in edges:
+            a = edge[0]
+            b = edge[1]
+            adj[a].append(b)
+            adj[b].append(a)
+        
+        q = deque([(0, -1)])
+        visit.add(0)
+        cycle = False
+
+        while q: 
+            element, parent = q.popleft()
+            for ngbr in adj[element]:
+                if ngbr == parent:
+                    continue
+                if ngbr in visit:
+                    return False 
+                else:
+                    q.append((ngbr, element))
+                    visit.add(ngbr)
+        
+        if cycle == False and len(visit) == n:
+            return True 
+        return False
+
+
+
+
 
