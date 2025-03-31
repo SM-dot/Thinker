910e0a5f1aa75b93ab1e279566e3ad8bac0ea3c7
SM-dot <66779569+SM-dot@users.noreply.github.com>
2025-03-31 14:44:45 -0400

    Create DesigningARandomisedSet.py

---
+++ DesigningARandomisedSet.py
@@ -0,0 +1,54 @@
+# Leetcode Link: https://leetcode.com/problems/insert-delete-getrandom-o1/description/?envType=study-plan-v2&envId=top-interview-150
+# Category: Data Structures and Maps and Lists 
+class RandomizedSet:
+    '''
+    Let's say we use an array. 
+    In an array/ list inserting is O(1) time as you insert it at the back. 
+    Getting random is also O(1) because you generate a random index and return the element at that index, since lookup in lists in python is also O(1) this operation is also O(1)
+    However, for removing - you need to find the index in which the element is and then also move other indexes to fill that gap which is O(n+n) - fidning index and moving other elements
+
+    Solution for removing - in addition to the array store a hashmap that has the value mapped to the index. 
+    lets say you have the element to remove at index i, swap it with the last element in the list. Then pop the last element from the list and update the map accordingly - all this will be done in O(1)
+
+    Now lets code! 
+    '''
+
+    def __init__(self):
+        self.map = defaultdict(int)
+        self.lst = []
+        
+
+    def insert(self, val: int) -> bool:
+        if val not in self.lst:
+            self.lst.append(val)
+            self.map[val] = len(self.lst) - 1
+            return True
+        return False
+
+        
+
+    def remove(self, val: int) -> bool:
+        if val not in self.lst:
+            return False 
+        
+        index = self.map[val]
+        lastelement = self.lst[len(self.lst) - 1]
+        self.lst[index] = lastelement
+        self.lst.pop() # O(1)
+
+        self.map[lastelement] = index
+        del self.map[val]
+        return True 
+        
+
+    def getRandom(self) -> int:
+        random_index  = random.randint(0, len(self.lst) - 1)
+        return self.lst[random_index]
+        
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
