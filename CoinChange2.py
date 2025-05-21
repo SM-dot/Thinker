f99ed5d7a433f8b96fac0a9e85ae1ebe57713af9
SM-dot <66779569+SM-dot@users.noreply.github.com>
2025-05-20 21:47:12 -0400

    Update CoinChange2.py

--- CoinChange2.py
+++ CoinChange2.py
@@ -3,17 +3,32 @@
 
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
+        """
+        We use a 2D table `t` where t[i][j] represents the number of ways to make amount `j` using the first `i` coins.
+
+        Base Case:
+        - There's exactly 1 way to make amount 0 (using no coins), so t[i][0] = 1 for all i.
+
+        Transition:
+        - If the current coin value is less than or equal to the current amount `j`,
+          we add two cases:
+            1. Including the coin: t[i][j - coin] (given in the question that we have an infinite number of coins)
+            2. Excluding the coin: t[i-1][j]
+        - Otherwise, we carry forward the previous value: t[i][j] = t[i-1][j]
+
+        Time Complexity: O(n * amount)
+        Space Complexity: O(n * amount)
+        """
         n = len(coins)
         t = [[0 for j in range(amount + 1)] for i in range(n + 1)]
 
         for i in range(n + 1):
             t[i][0] = 1
-        
 
         for i in range(1, n + 1):
             for j in range(1, amount + 1):
                 if coins[i - 1] <= j:
-                    t[i][j] = t[i][j - coins[i-1]] + t[i-1][j]
+                    t[i][j] = t[i][j - coins[i - 1]] + t[i - 1][j]
                 else:
-                    t[i][j] = t[i-1][j]
+                    t[i][j] = t[i - 1][j]
         return t[n][amount]
