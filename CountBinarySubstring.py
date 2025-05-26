31e64f5ef2bd1c8bf112c8a849f0654f90db4584
SM-dot <66779569+SM-dot@users.noreply.github.com>
2025-05-26 15:56:47 -0400

    Update CountBinarySubstring.py

--- CountBinarySubstring.py
+++ CountBinarySubstring.py
@@ -33,3 +33,34 @@ class Solution:
         for i in range(0, len(groups) - 1):
             answer += min(groups[i], groups[i + 1])
         return answer 
+# OPTIMIZED CODE: O(1) Space Complexity
+class Solution:
+    def countBinarySubstrings(self, s: str) -> int:
+        '''
+        Main idea: 
+        find the number of groups, and for each group how many 0's and 1's 
+        "001100111"
+        keeping count of consecutive 1's 
+        grouping: "00", "11", "00", "111"
+                    2    2.     2.    3
+
+        now, to sount substring, ans += min(group[i], group[i + 1])
+        cause 0011 -> has 2 subsets 01, 0011 = 2 susbets
+        000111 -> has 3 subsets 01, 0011, 000111 = 3 subsets 
+        if you have 00111 -> 01, 0011 = 2 subsets 
+
+        '''
+
+        prev, curr, answer = 0, 1, 0
+        groups = []
+        n = len(s)
+
+        for i in range(1, n):
+            if s[i] == s[i - 1]:
+                curr += 1
+            else: 
+                answer += min(prev, curr)
+                prev, curr = curr, 1
+        return answer + min(prev, curr)
+
+
