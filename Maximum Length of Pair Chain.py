f1325469b9d7ea15f0858aece060ea04a3383c07
SM-dot <syonamehraa@gmail.com>
2025-07-11 00:50:09 -0400

    Maximum Lenght of Pair Chain added

--- Maximum Length of Pair Chain.py
+++ Maximum Length of Pair Chain.py
@@ -4,6 +4,15 @@
 
 class Solution:
     def findLongestChain(self, pairs: List[List[int]]) -> int:
+        '''
+        Recursion + Memoization code, TC: O(N^2), SC: O(N^2) + O(N) recursion stack
+        Sort the pairs based on the first element of each pair
+        For each pair, we have 2 choices - take or skip
+        If taking, check if the current pair can be chained with the previous pair taken
+        If skipping, simply move to the next pair
+        
+        Can be solved more optimallty using Greedy approach - TC: O(N log N), SC: O(1)
+        '''
         pairs.sort()
         n = len(pairs)
         t = [[-1 for _ in range(n+1)] for _ in range(n+1)]
