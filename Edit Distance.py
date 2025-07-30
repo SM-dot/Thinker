c366d6b29d9194efbd02a34cbb27f90bceb004b1
SM-dot <syonamehraa@gmail.com>
2025-07-30 19:00:25 -0400

    Optimised with bottom up now

--- Edit Distance.py
+++ Edit Distance.py
@@ -57,3 +57,38 @@ class Solution:
             return dp[i][j]
         
         return solve(0, 0)
+    
+    # Bottom-up approach
+    class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        '''
+        Bottom Up, build from right to left recursive solution
+        almost a direct susbtitute of i and j
+        Operations: 
+        - Create a DP table where dp[i][j] represents the minimum edit distance to convert  
+        word1[0..i] to word2[0..j]
+        - Fill the DP table based on the three operations: insert, delete, and replace
+        - The base cases are when one of the strings is empty, in which case the distance
+        is the length of the other string
+        Output: The minimum edit distance between word1 and word2
+        T.C: O(N * M) where N is the length of word1 and M is the length of word2
+        S.C: O(N * M) for the DP table
+        Using Dynamic Programming to calculate the minimum edit distance
+        '''
+        n = len(word1)
+        m = len(word2)
+        # tells us the min operations required to convert word1 of len i to word2 of len j
+        t = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+
+        for i in range(n + 1):
+            for j in range(m + 1):
+                if i == 0 or j == 0:
+                    t[i][j] = i + j
+                elif (word1[i - 1] == word2[j - 1]):
+                    t[i][j] = t[i-1][j - 1]
+                else:
+                    insert = t[i][j-1]
+                    delete = t[i-1][j]
+                    replace = t[i-1][j-1]
+                    t[i][j] = 1 + min(insert, delete, replace)
+        return t[n][m]
