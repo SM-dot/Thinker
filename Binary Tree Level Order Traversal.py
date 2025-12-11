429d0ce133a908efae6d168d56ccaf7c1d6e4da4
SM-dot <syonamehraa@gmail.com>
2025-12-11 00:13:34 -0500

    Rev, optimised code added with explanation

--- Binary Tree Level Order Traversal.py
+++ Binary Tree Level Order Traversal.py
@@ -48,3 +48,40 @@ class Solution:
             
             result.append(level)
         return result[::-1] # reversing the result to get bottom to top order, time complexity O(n)
+    
+
+# More explanation code, revised on December 10, 2025
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        '''
+        super straightforward implementation of a breadtn first traversal here. 
+        Basically, replace the nextnode with just chekcing the left and right nodes and we do not need to keep a visited array cause it is a tree and only works in one direction. For optimization instead of storing the full level of each level of the tree we are just appending the last value to the tree. This helps ion saving the memory storagr that we have. 
+        Time Complexity: O(V + E) (simple TC of BFS)
+        Space Complexity: O(N) (just the storage amount of the q and the answer) Since we optimised by not storing the entire level this is the best we can do here. 
+        '''
+        q = deque()
+        q.append(root)
+
+        if not root:
+            return []
+
+        answer = []
+        while q:
+            k = len(q)
+            for i in range(k):
+                node = q.popleft()
+                if i == k - 1:
+                    answer.append(node.val)
+                
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+        return answer 
